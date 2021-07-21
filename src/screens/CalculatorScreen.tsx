78ca5897040abd4867b7649e58beabea95d754a5
ChristianRV29 <kmylorv@gmail.com>
2021-07-21 06:51:06 -0500

    Final version

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,115 +1,21 @@
-import React, {Fragment, useState, useRef} from 'react';
+import React, {Fragment} from 'react';
 import {View, Text} from 'react-native';
 import {ButtonMdc} from '../components/molecules/ButtonMdc';
 import {appStyles} from './../styles/styles';
 
-enum Operators {
-  add,
-  substract,
-  multiply,
-  divide,
-}
+import {useCalculator} from './../hooks/useCalculator';
 
 export const CalculatorScreen = () => {
-  const [operatorNumber, setOperatorNumber] = useState('0');
-  const [previosNumber, setPreviosNumber] = useState('0');
-
-  const lastOperation = useRef<Operators>();
-
-  const cleanNumber = () => {
-    setOperatorNumber('0');
-    setPreviosNumber('0');
-  };
-
-  const buildNumber = (numberText: string) => {
-    if (operatorNumber.includes('.') && numberText === '.') {
-      return;
-    }
-    if (operatorNumber.startsWith('0') || operatorNumber.startsWith('-0')) {
-      if (numberText === '.') {
-        setOperatorNumber(operatorNumber.concat(numberText));
-      } else if (numberText === '0' && operatorNumber.includes('.')) {
-        setOperatorNumber(operatorNumber.concat(numberText));
-      } else if (numberText !== '0' && !operatorNumber.includes('.')) {
-        setOperatorNumber(numberText);
-      } else if (numberText === '0' && !operatorNumber.includes('.')) {
-        setOperatorNumber(operatorNumber);
-      } else {
-        setOperatorNumber(operatorNumber.concat(numberText));
-      }
-    } else {
-      setOperatorNumber(operatorNumber.concat(numberText));
-    }
-  };
-
-  const positiveNegative = () => {
-    if (operatorNumber.includes('-')) {
-      setOperatorNumber(operatorNumber.replace('-', ''));
-    } else {
-      setOperatorNumber('-'.concat(operatorNumber));
-    }
-  };
-
-  const delNumber = () => {
-    if (
-      operatorNumber.length === 1 ||
-      (operatorNumber.length === 2 && operatorNumber.includes('-'))
-    ) {
-      return setOperatorNumber('0');
-    }
-    setOperatorNumber(num => num.slice(0, -1));
-  };
-
-  const castPreviousNumber = () => {
-    if (operatorNumber.endsWith('.')) {
-      setPreviosNumber(operatorNumber.slice(0, -1));
-    } else {
-      setPreviosNumber(operatorNumber);
-    }
-
-    setOperatorNumber('0');
-  };
-
-  const btnOperation = (operator: string) => {
-    castPreviousNumber();
-    switch (operator) {
-      case '/':
-        lastOperation.current = Operators.divide;
-        break;
-      case '*':
-        lastOperation.current = Operators.multiply;
-        break;
-      case '-':
-        lastOperation.current = Operators.substract;
-        break;
-      case '+':
-        lastOperation.current = Operators.add;
-        break;
-    }
-  };
-
-  const calculate = () => {
-    const number1 = Number(operatorNumber);
-    const number2 = Number(previosNumber);
-
-    switch (lastOperation.current) {
-      case Operators.add:
-        setOperatorNumber(`${number1 + number2}`);
-        break;
-      case Operators.substract:
-        setOperatorNumber(`${number2 - number1}`);
-        break;
-      case Operators.multiply:
-        setOperatorNumber(`${number1 * number2}`);
-        break;
-      case Operators.divide:
-        setOperatorNumber(`${number2 / number1}`);
-        break;
-      default:
-        break;
-    }
-    setPreviosNumber('0');
-  };
+  const {
+    previosNumber,
+    operatorNumber,
+    cleanNumber,
+    positiveNegative,
+    delNumber,
+    buildNumber,
+    btnOperation,
+    calculate,
+  } = useCalculator();
 
   return (
     <Fragment>
